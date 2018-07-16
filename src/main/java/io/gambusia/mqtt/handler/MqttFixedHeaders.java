3010c57fc210b4d18601f573cb9eee9915a91f44
Issey Yamakoshi <r16turbo@gmail.com>
2018-07-16 22:08:06 +0900

    Change from interface to class

--- src/main/java/io/gambusia/mqtt/handler/MqttFixedHeaders.java
+++ src/main/java/io/gambusia/mqtt/handler/MqttFixedHeaders.java
@@ -20,44 +20,45 @@ import io.netty.handler.codec.mqtt.MqttFixedHeader;
 import io.netty.handler.codec.mqtt.MqttMessageType;
 import io.netty.handler.codec.mqtt.MqttQoS;
 
-public interface MqttFixedHeaders {
+public final class MqttFixedHeaders {
+  private MqttFixedHeaders() {}
 
-  MqttFixedHeader CONNECT_HEADER =
+  public static final MqttFixedHeader CONNECT_HEADER =
       new MqttFixedHeader(MqttMessageType.CONNECT, false, MqttQoS.AT_MOST_ONCE, false, -1);
 
-  MqttFixedHeader CONNACK_HEADER =
+  public static final MqttFixedHeader CONNACK_HEADER =
       new MqttFixedHeader(MqttMessageType.CONNACK, false, MqttQoS.AT_MOST_ONCE, false, 2);
 
-  MqttFixedHeader PUBACK_HEADER =
+  public static final MqttFixedHeader PUBACK_HEADER =
       new MqttFixedHeader(MqttMessageType.PUBACK, false, MqttQoS.AT_MOST_ONCE, false, 2);
 
-  MqttFixedHeader PUBREC_HEADER =
+  public static final MqttFixedHeader PUBREC_HEADER =
       new MqttFixedHeader(MqttMessageType.PUBREC, false, MqttQoS.AT_MOST_ONCE, false, 2);
 
-  MqttFixedHeader PUBREL_HEADER =
+  public static final MqttFixedHeader PUBREL_HEADER =
       new MqttFixedHeader(MqttMessageType.PUBREL, false, MqttQoS.AT_LEAST_ONCE, false, 2);
 
-  MqttFixedHeader PUBCOMP_HEADER =
+  public static final MqttFixedHeader PUBCOMP_HEADER =
       new MqttFixedHeader(MqttMessageType.PUBCOMP, false, MqttQoS.AT_MOST_ONCE, false, 2);
 
-  MqttFixedHeader SUBSCRIBE_HEADER =
+  public static final MqttFixedHeader SUBSCRIBE_HEADER =
       new MqttFixedHeader(MqttMessageType.SUBSCRIBE, false, MqttQoS.AT_LEAST_ONCE, false, -1);
 
-  MqttFixedHeader SUBACK_HEADER =
+  public static final MqttFixedHeader SUBACK_HEADER =
       new MqttFixedHeader(MqttMessageType.SUBACK, false, MqttQoS.AT_MOST_ONCE, false, -1);
 
-  MqttFixedHeader UNSUBSCRIBE_HEADER =
+  public static final MqttFixedHeader UNSUBSCRIBE_HEADER =
       new MqttFixedHeader(MqttMessageType.UNSUBSCRIBE, false, MqttQoS.AT_LEAST_ONCE, false, -1);
 
-  MqttFixedHeader UNSUBACK_HEADER =
+  public static final MqttFixedHeader UNSUBACK_HEADER =
       new MqttFixedHeader(MqttMessageType.UNSUBACK, false, MqttQoS.AT_MOST_ONCE, false, 2);
 
-  MqttFixedHeader PINGREQ_HEADER =
+  public static final MqttFixedHeader PINGREQ_HEADER =
       new MqttFixedHeader(MqttMessageType.PINGREQ, false, MqttQoS.AT_MOST_ONCE, false, 0);
 
-  MqttFixedHeader PINGRESP_HEADER =
+  public static final MqttFixedHeader PINGRESP_HEADER =
       new MqttFixedHeader(MqttMessageType.PINGRESP, false, MqttQoS.AT_MOST_ONCE, false, 0);
 
-  MqttFixedHeader DISCONNECT_HEADER =
+  public static final MqttFixedHeader DISCONNECT_HEADER =
       new MqttFixedHeader(MqttMessageType.DISCONNECT, false, MqttQoS.AT_MOST_ONCE, false, 0);
 }
