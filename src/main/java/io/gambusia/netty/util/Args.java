afb1fe31910e75fbd46be66fb89417ea0e4f1563
Issey Yamakoshi <r16turbo@gmail.com>
2018-07-18 21:12:51 +0900

    Change to implementation without dependency

--- src/main/java/io/gambusia/netty/util/Args.java
+++ src/main/java/io/gambusia/netty/util/Args.java
@@ -19,60 +19,76 @@ package io.gambusia.netty.util;
 public final class Args {
   private Args() {}
 
-  public static <T> T checkNotNull(T arg, String text) {
-    return io.netty.util.internal.ObjectUtil.checkNotNull(arg, text);
-  }
-
   public static int checkPositive(int i, String name) {
-    return io.netty.util.internal.ObjectUtil.checkPositive(i, name);
+    if (i <= 0) {
+      throw new IllegalArgumentException(String.format("%s: %d (expected: > 0)", name, i));
+    }
+    return i;
   }
 
   public static long checkPositive(long i, String name) {
-    return io.netty.util.internal.ObjectUtil.checkPositive(i, name);
+    if (i <= 0) {
+      throw new IllegalArgumentException(String.format("%s: %d (expected: > 0)", name, i));
+    }
+    return i;
   }
 
   public static int checkPositiveOrZero(int i, String name) {
-    return io.netty.util.internal.ObjectUtil.checkPositiveOrZero(i, name);
+    if (i < 0) {
+      throw new IllegalArgumentException(String.format("%s: %d (expected: >= 0)", name, i));
+    }
+    return i;
   }
 
   public static long checkPositiveOrZero(long i, String name) {
-    return io.netty.util.internal.ObjectUtil.checkPositiveOrZero(i, name);
-  }
-
-  public static <T> T[] checkNonEmpty(T[] array, String name) {
-    return io.netty.util.internal.ObjectUtil.checkNonEmpty(array, name);
-  }
-
-  public static int intValue(Integer wrapper, int defaultValue) {
-    return io.netty.util.internal.ObjectUtil.intValue(wrapper, defaultValue);
-  }
-
-  public static long longValue(Long wrapper, long defaultValue) {
-    return io.netty.util.internal.ObjectUtil.longValue(wrapper, defaultValue);
-  }
-
-  public static String checkNotEmpty(String arg, String text) {
-    io.netty.util.internal.ObjectUtil.checkNotNull(arg, text);
-    if (arg.isEmpty()) {
-      throw new IllegalArgumentException(text);
+    if (i < 0) {
+      throw new IllegalArgumentException(String.format("%s: %d (expected: >= 0)", name, i));
     }
-    return arg;
-  }
-
-  public static <T> T[] checkNotContainsNull(T[] array, String name) {
-    checkNonEmpty(array, name);
-    for (int i = 0; i < array.length; i++) {
-      if (array[i] == null) {
-        throw new NullPointerException(name + "[" + i + "]");
-      }
-    }
-    return array;
+    return i;
   }
 
   public static int checkUnsignedShort(int i, String name) {
     if ((i & 0xFFFF0000) != 0) {
-      throw new IllegalArgumentException(name + ": " + i + " (expected: >= 0 && <= 65535)");
+      throw new IllegalArgumentException(String.format("%s: %d (expected: 0–65535)", name, i));
     }
     return i;
+  }
+
+  public static long checkUnsignedInt(long i, String name) {
+    if ((i & 0xFFFFFFFF00000000L) != 0) {
+      throw new IllegalArgumentException(String.format("%s: %d (expected: 0–4294967295)", name, i));
+    }
+    return i;
+  }
+
+  public static <T> T checkNotNull(T arg, String text) {
+    if (arg == null) {
+      throw new NullPointerException(text);
+    }
+    return arg;
+  }
+
+  public static String checkNotEmpty(String arg, String name) {
+    if (checkNotNull(arg, name).isEmpty()) {
+      throw new IllegalArgumentException(String.format("%s is empty", name));
+    }
+    return arg;
+  }
+
+  public static <T> T[] checkNotEmpty(T[] array, String name) {
+    if (checkNotNull(array, name).length <= 0) {
+      throw new IllegalArgumentException(String.format("%s is empty", name));
+    }
+    return array;
+  }
+
+  public static <T> T[] checkNotContainsNull(T[] array, String name) {
+    checkNotEmpty(array, name);
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == null) {
+        throw new NullPointerException(String.format("%s[%d]", name, i));
+      }
+    }
+    return array;
   }
 }
